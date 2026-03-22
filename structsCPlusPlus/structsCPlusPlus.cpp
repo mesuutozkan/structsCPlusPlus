de25ecfa0dff9cdf6e5965f064feef9a1ae0a657
Mesut Ozkan Ertok <mesutozkanertok@gmail.com>
2026-03-22 18:00:44 +0300

    Structs in function has been added.

--- structsCPlusPlus/structsCPlusPlus.cpp
+++ structsCPlusPlus/structsCPlusPlus.cpp
@@ -10,6 +10,31 @@ struct Student {		//Common using of struct names starts with upper case characte
 	double sGradeAverage;
 };
 
+void decidePassedOrNot(Student &students)		//decider of passing status (by using call by reference, to not execute in local function)
+{
+	cout << " Student status: ";
+	students.sGradeAverage >= 50 ? cout << "PASSED" << endl : cout << "NOT PASSED" << endl;		//decider block
+}
+
+void decideStudentsExam(Student Students[], int size)		//decide all students status, array comes with its index automatically by calling it array[]
+{
+	cout << "Status Of Students" << endl;
+
+	cout << endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		//track student by its index (i + 1)
+
+		cout << i + 1 << ". Student number: " << (Students + i) -> sNumber << " "
+			<< " Student name: " << (Students + i) -> sName << " " 
+			<< " Student Average Grade: " << (Students + i) -> sGradeAverage;
+
+		decidePassedOrNot(Students[i]);		//use decider function (function in function
+		cout << endl;
+	}
+}
+
 int main()
 {
 	int n;		//number of how many students desired
@@ -45,18 +70,51 @@ int main()
 		
 		cout << "Enter " << i + 1 << ". students average grade: ";
 		cin >> (PtrStudents + i) -> sGradeAverage;
+		cout << endl;
 	}
 
-	cout << endl << "List Of Students\n" << endl;
+	//check allocation
+
+	cout << "List Of Students\n" << endl;
 
 	for (int i = 0; i < n; i++)
 	{
-		cout << i + 1 << ". Students number: " << (PtrStudents + i) -> sNumber << endl;
-		cout << i + 1 << ". Students name: " << (PtrStudents + i) -> sName << endl;
-		cout << i + 1 << ". Students average grade: " << (PtrStudents + i) -> sGradeAverage << endl;
+		cout << i + 1 << ". Student number: " << (PtrStudents + i) -> sNumber;
+		cout << " " << "Student name: " << (PtrStudents + i)->sName;
+		cout  << " " << "Student average grade: " << (PtrStudents + i)->sGradeAverage << endl;
+		cout << endl;
 	}
-	delete[] PtrStudents;
-	PtrStudents = NULL;
 
+	cout << endl;
 
+	decideStudentsExam(PtrStudents, n);		//decide with PtrStudents, not locally
+
+	delete[] PtrStudents;		//release PtrStudents
+	PtrStudents = NULL;			//ensure it is not point any memory address
+
+	return 0;
 }
+/*
+output:
+Enter student count: 2
+Enter 1. students number: 11
+Enter 1. students name: aa aa
+Enter 1. students average grade: 49
+
+Enter 2. students number: 22
+Enter 2. students name: bb bb
+Enter 2. students average grade: 51
+
+List Of Students
+
+1. Student number: 11 Student name: aa aa Student average grade: 49
+
+2. Student number: 22 Student name: bb bb Student average grade: 51
+
+
+Status Of Students
+
+1. Student number: 11  Student name: aa aa  Student Average Grade: 49 Student status: NOT PASSED
+
+2. Student number: 22  Student name: bb bb  Student Average Grade: 51 Student status: PASSED
+*/
